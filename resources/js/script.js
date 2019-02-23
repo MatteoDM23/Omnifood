161df9c06f92152cef597d4bb30ecc0b89f6b9e8
matt90 <matteodemichele84@gmail.com>
2019-02-23 23:59:09 +0100

    all HTML, CSS, jQuery done

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,6 +11,9 @@ $(document).ready(function() {
         offset: '60px;'   
     });
 
+    
+
+    
     /* scroll on buttons */
     $('.js--scroll-to-plans').click(function() {
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
@@ -19,6 +22,8 @@ $(document).ready(function() {
     $('.js--scroll-to-start').click(function() {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
+
+
 
     // Select all links with hashes
     $('a[href*="#"]')
@@ -57,6 +62,24 @@ $(document).ready(function() {
         }
     });
 
+    // /* Navigation scroll */
+    // $(function() {
+    //     $('a[href*=#]:not([href=#])').click(function() {
+    //       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    //         var target = $(this.hash);
+    //         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    //         if (target.length) {
+    //           $('html,body').animate({
+    //             scrollTop: target.offset().top
+    //           }, 1000);
+    //           return false;
+    //         }
+    //       }
+    //     });
+    //   });
+
+
+
 
     // animations //
     $('.js--wp-1').waypoint(function(direction) {
@@ -83,4 +106,25 @@ $(document).ready(function() {
         offset: '70%'
     });
 
+
+
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }        
+    });
+
+
+
+    
 });
